6b1feaf3530a97c8e4c50599dd4e65399d7779ea
santu7212 <santupramanik540@gmail.com>
2025-10-05 12:11:09 +0530

    user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,44 +1,48 @@
-import mongoose ,{Schema}from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import { Profiler } from "react";
 
+const userSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    bio: {
+      type: String,
+      default: "Hey there! I am using Xpose",
+    },
+    Profile_picture: {
+      type: String,
+      default: "",
+    },
+    cover_photo: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    followers: [{ type: String, ref: "User" }],
+    following: [{ type: String, ref: "User" }],
+    connections: [{ type: String, ref: "User" }],
+  },
+  { timestamps: true, minimize: false }
+);
 
-const userSchema= new Schema({
-    _id:{
-        type:String,
-        required:true,
-    },
-    email:{
-        type:String,
-        required:true,
+const User = mongoose.model("User", userSchema);
 
-
-    },
-    fullName:{
-        type:String,
-        required:true
-    },
-    userName:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    bio:{
-        type:String,
-        default:"Hey there! I am using Xpose"
-    },
-    Profile_picture:{
-        type:String,
-        default:""
-    },
-    cover_photo:{
-        type:String,
-        default:"",
-
-    },
-    location:{
-        type:String,
-        default:""
-    },
-    followers
-
-},{})
+export default User;
