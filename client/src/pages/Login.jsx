605dc71db107a66cacb566600d0353258d49ca0d
santu7212 <santupramanik540@gmail.com>
2025-10-07 12:04:13 +0530

    logo adjusted

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,21 +12,34 @@ const Login = () => {
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       />
 
-      {/* Left side branding  */}
-      <div className="flex-1 flex flex-col items-start justify-between p-6 md:p10 lg:pl-40">
-        <img src={assets.logo} alt="" className="h-12 object-contain" />
+      {/* Left side branding */}
+      <div className="flex-1 flex flex-col items-start justify-between p-6 md:p-10 lg:pl-40">
+        <div className="flex items-center gap-3">
+          <img src={assets.logo} alt="logo" className="h-12 object-contain" />
+          <h1 className="text-3xl font-extrabold tracking-wide">
+            <span className="text-blue-500">Pl</span>
+            <span className="text-red-500">ixa</span>
+          </h1>
+        </div>
+
         <div>
-          <div className="flex items-center gap-3 mb-4 max-md:mt:10">
+          <div className="flex items-center gap-3 mb-4 max-md:mt-10">
             <img src={assets.group_users} alt="" className="h-8 md:h-10" />
             <div>
               <div className="flex">
-                {Array(5).fill(0).map((_,i)=>(<Star key={i} className="size-4 md:size-4.5 text-transparent fill-amber-500"/>))}
+                {Array(5)
+                  .fill(0)
+                  .map((_, i) => (
+                    <Star
+                      key={i}
+                      className="size-4 md:size-4.5 text-transparent fill-amber-500"
+                    />
+                  ))}
               </div>
-              <p>Used by 12k developers </p>
+              <p>Used by 12k developers</p>
             </div>
           </div>
         </div>
-
       </div>
     </div>
   );
