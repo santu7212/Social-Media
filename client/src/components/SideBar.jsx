f37321e8eb60f5805b725d38962c227a889d1987
santu7212 <santupramanik540@gmail.com>
2025-10-08 19:26:22 +0530

    add user profile section

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,11 +1,14 @@
- import React from "react";
-import { assets } from "../assets/assets.js";
+import React from "react";
+import { assets, dummyUserData } from "../assets/assets.js";
 import { Link, useNavigate } from "react-router-dom";
 import MenuItems from "./MenuItems.jsx";
-import { CirclePlus } from "lucide-react";
+import { CirclePlus, LogOut } from "lucide-react";
+import { UserButton, useClerk } from "@clerk/clerk-react";
 
 const SideBar = ({ sideBarOpen, setSideBarOpen }) => {
   const navigate = useNavigate();
+  const user = dummyUserData;
+  const { signOut } = useClerk();
 
   return (
     <div
@@ -29,13 +32,30 @@ const SideBar = ({ sideBarOpen, setSideBarOpen }) => {
         <hr className="border-gray-300 mb-8" />
         <MenuItems setSideBArOpen={setSideBarOpen} />
 
-        <Link to="/create-post" className="flex items-center justify-center gap-2 py-2.5 mt-6 mx-6 rounded-lg 
+        <Link
+          to="/create-post"
+          className="flex items-center justify-center gap-2 py-2.5 mt-6 mx-6 rounded-lg 
         bg-gradient-to-r from-indigo-500 to-purple-600
          hover:from-indigo-700 hover:to-purple-800 active: scale-95
-          transition text-white cursor-pointer">
-        <CirclePlus className="w-5 h-5 "/>
-        Creater Post 
+          transition text-white cursor-pointer"
+        >
+          <CirclePlus className="w-5 h-5 " />
+          Creater Post
         </Link>
+      </div>
+
+      <div className="w-full border-t border-grey-200 p-4 px-7 flex items-center justify-between">
+        <div className="flex gap-2 items-center cursor-pointer">
+          <UserButton />
+          <div>
+            <h1 className="text-sm font-medium">{user.full_name}</h1>
+            <p className="text-sm text-grey-500">@{user.username}</p>
+          </div>
+        </div>
+        <LogOut
+          onClick={signOut}
+          className="w-4.5 text-grey-400 hover:text-grey-700 transition cursor-pointer"
+        />
       </div>
     </div>
   );
