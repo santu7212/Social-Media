96e44833fa5adf4fa8d748328242410f2b93c854
santu7212 <santupramanik540@gmail.com>
2025-10-08 10:45:07 +0530

    add menue items

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { assest } from "../assets/assets.js";
+ import React from "react";
+import { assets } from "../assets/assets.js";
 import { useNavigate } from "react-router-dom";
+import MenuItems from "./MenuItems.jsx";
 
-const SideBar = ({ sideBarOpen, setSideBArOpen }) => {
+const SideBar = ({ sideBarOpen, setSideBarOpen }) => {
   const navigate = useNavigate();
+
   return (
     <div
-      className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items
-        -center max-sm:absolute top-0 bottom-0 z-20 ${
-          sideBarOpen ? " translate-x-0" : "max-sm: translate-x-full"
-        } transition-all duration-300 ease-in-out`}
+      className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20 ${
+        sideBarOpen ? "translate-x-0" : "max-sm:translate-x-full"
+      } transition-all duration-300 ease-in-out`}
     >
       <div className="w-full">
         <img
-          onClick={() => {
-            navigate("/ ");
-          }}
-          src={assest.logo}
-          alt=""
+          onClick={() => navigate("/")}
+          src={assets.logo}
+          alt="logo"
           className="w-26 ml-7 my-2 cursor-pointer"
         />
-        <hr className="border-grey-300 mb-8" />
+        <hr className="border-gray-300 mb-8" />
+        <MenuItems setSideBArOpen={setSideBarOpen} />
       </div>
     </div>
   );
