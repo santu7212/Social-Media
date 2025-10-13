5199bd43ef10a01b81a86334ab377e8bdf5ba6fe
santu7212 <santupramanik540@gmail.com>
2025-10-13 10:59:17 +0530

    add onclick on eye and messages

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { dummyConnectionsData } from '../assets/assets'
 import { Eye, MessageSquare } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const Messages = () => {
+
+  const navigate=useNavigate()
   return (
     <div className='min-h-screen relative bg-slate-50 '> 
     <div className='max-w-6xl mx-auto p-6'>
@@ -29,13 +32,13 @@ const Messages = () => {
 
             <div className='flex flex-col gap-2 mt-4'>
 
-              <button className='size-10 flex item-center justify-center text-sm 
+              <button onClick={()=>navigate(`/messages/${user._id}`)} className='size-10 flex item-center justify-center text-sm 
               rounded-bg  bg-slate-100 hover:bg-slate-200 text-slate-800 
               active:scale-95 transition cursor-pointer gap-1'>
                 <MessageSquare className='w-4 h-4'/>
               </button>
 
-               <button className='size-10 flex item-center justify-center text-sm 
+               <button onClick={()=>navigate(`/profile/${user._id}`)} className='size-10 flex item-center justify-center text-sm 
               rounded-bg  bg-slate-100 hover:bg-slate-200 text-slate-800 
               active:scale-95 transition cursor-pointer '>
                 <Eye className='w-4 h-4'/>
