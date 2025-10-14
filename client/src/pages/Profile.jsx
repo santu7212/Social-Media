c2bff8c9ad25efd2d6af612cf9edbf28d47a48b7
santu7212 <santupramanik540@gmail.com>
2025-10-14 21:27:01 +0530

    change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,69 +1,51 @@
- import React, { useEffect, useState } from 'react';
-import { useParams } from "react-router-dom";
-import Loading from "../components/Loading";
-import { dummyPostsData, dummyUserData } from "../assets/assets";
-import UserProfileInfo from '../components/UserProfileInfo';
+import React, { useEffect, useState } from 'react'
+import {useParams} from "react-router-dom"
+import Loading from "../components/Loading"
+import {dummyPostsData,dummyUserData} from "../assets/assets"
+import UserProfileInfo from '../components/UserProfileInfo'
 
 const Profile = () => {
-  const { profileId } = useParams();
-  const [user, setUser] = useState(null);
-  const [posts, setPosts] = useState([]);
-  const [activeTab, setActiveTab] = useState("posts");
-  const [showEdit, setShowEdit] = useState("false");
+  const {profileId} =useParams()
+  const [user,setUser]=useState(null)
+  const [posts,setPosts]=useState([])
+  const[activeTab,setActiveTab]=useState("posts")
+  const [ showEdit,setShowEdit]=useState("false")
 
-  const fetchUser = async () => {
-    setUser(dummyUserData);
-    setPosts(dummyPostsData);
-  };
+  const fetchUser=async()=>{
+    setUser(dummyUserData)
+    setPosts(dummyPostsData)
+  }
 
-  useEffect(() => {
-    fetchUser();
-  }, []);
-
+  useEffect(()=>{
+    fetchUser()
+  },[])
   return user ? (
-    <div className="relative h-full overflow-y-scroll bg-gradient-to-b from-gray-50 via-gray-100 to-gray-200 p-6">
-      <div className="max-w-3xl mx-auto">
-        {/* Profile Card */}
-        <div className="bg-white rounded-2xl shadow-md overflow-hidden border border-gray-100 hover:shadow-xl transition-all duration-300">
-          
-          {/* Cover Photo */}
-          <div className="relative h-40 md:h-56 bg-gradient-to-r from-blue-600 via-purple-500 to-red-500">
-            {user.cover_photo && (
-              <img
-                src={user.cover_photo}
-                alt="Cover"
-                className="w-full h-full object-cover"
-              />
-            )}
-            <div className="absolute inset-0 bg-black/10"></div>
+    <div className='relative h-full overflow-y-scroll bg-grey-50 p-6'>
+      <div className=' max-w 3xl max-auto'>
+        {/* profile card  */}
+        <div className='bg-white ropunded-2xl shadow overflow-hidden'>
+          {/* cover Photo  */}
+          <div className='h-40 md:h-56 bg-gradient-to-r from-indigo-200 via-purple-200 
+           to-pink-200 '>
 
-            {/* Profile Picture overlapping cover */}
-            <div className="absolute left-6 md:left-8 bottom-0 transform translate-y-1/2">
-              <div className="w-32 h-32 rounded-full overflow-hidden ring-4 ring-white shadow-lg bg-gray-100">
-                <img
-                  src={user.profile_picture}
-                  alt="Profile"
-                  className="w-full h-full object-cover"
-                />
-              </div>
-            </div>
+            {user.cover_photo && <img src={user.cover_photo} alt='' className='
+            w-full h-full object-cover'/>}
+
           </div>
 
-          {/* User Info below profile picture */}
-          <div className="px-6 md:px-8 mt-20">
-            <UserProfileInfo user={user} />
-          </div>
+          {/* user Info  */}
+          <UserProfileInfo user={user} posts={posts} profileId={profileId}
+          setShowEdit={setShowEdit}/>
 
-          {/* Posts or other content */}
-          <div className="p-6 pt-6">
-            {/* Here you can render posts or tabs */}
-          </div>
+
+
         </div>
-      </div>
-    </div>
-  ) : (
-    <Loading />
-  );
-};
 
-export default Profile;
+      </div>
+      
+    </div>
+  ):
+  (<Loading/>)
+}
+
+export default Profile
