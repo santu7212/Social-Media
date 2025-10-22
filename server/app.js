91d11e15cd2b6a539c9342a45c6c427f082f9bed
santu7212 <santupramanik540@gmail.com>
2025-10-22 20:15:57 +0530

    fix app

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => res.send("server is running bro"));
+app.get("/", (req, res) => res.send("server is running of plixa"));
 app.use("/api/ingest", serve({ client: inngest, functions }))
 
 export default app;
