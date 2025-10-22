45eb5f4d5211f36c84b657775ba075d6e551acd7
santu7212 <santupramanik540@gmail.com>
2025-10-22 21:08:47 +0530

    Xpose

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     mongoose.connection.on("connected", () =>console.log("database connected"));
-    await mongoose.connect(`${process.env.MONGODB_URI}/Plixa`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/Xpose`);
   } catch (error) {
     console.log(error.message);
   }
