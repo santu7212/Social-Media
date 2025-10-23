a7bdf3138e82aab8a6f1260b66e07ab73dc1ba0f
santu7212 <santupramanik540@gmail.com>
2025-10-23 20:26:05 +0530

    fix index for inngest

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -10,7 +10,8 @@ const syncUserCreation=inngest.createFunction(
     {event:"clerk/user.created"},
     async({event})=>{
         const {id,first_name,last_name,email_addresses,image_url}=event.data
-        let username=email_addresses[0].email_addresses.split("@")[0]
+         let username = email_addresses[0].email_address.split("@")[0]
+
         // Check availability of user name 
         const user=await User.findOne({username})
         if(user){
@@ -19,7 +20,8 @@ const syncUserCreation=inngest.createFunction(
 
         const userData={
             _id:id,
-            email:email_addresses[0].email_addresses,
+            email: email_addresses[0].email_address,
+
             full_name:first_name + " " + last_name,
             profile_picture:image_url,
             username
@@ -39,7 +41,8 @@ const syncUserUpdation=inngest.createFunction(
         const {id,first_name,last_name,email_addresses,image_url}=event.data
         
         const updateUserData={
-            email:email_addresses[0].email_addresses,
+             email: email_addresses[0].email_address,
+
             full_name:first_name + " " + last_name,
             profile_picture:image_url
         } 
