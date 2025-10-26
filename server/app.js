c899eddd5881daaad4c65a44796e288205954f86
santu7212 <santupramanik540@gmail.com>
2025-10-26 18:59:27 +0530

    comment out clerk middleware

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { serve } from "inngest/express";
 import {inngest,functions} from "./inngest/index.js"
-import { clerkMiddleware } from '@clerk/express'
+// import { clerkMiddleware } from '@clerk/express'
 // import userRouter from "./routes/user.routes.js";
 
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(clerkMiddleware())
+// app.use(clerkMiddleware())
 
 app.get("/", (req, res) => res.send("server is running for plixa"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
